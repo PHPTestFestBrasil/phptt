0f612b2deb68e8e2fdfbc5455a143b061ceead4f
Airton Zanon <airtonzanon@gmail.com>
2017-11-11 15:50:38 -0200

    Escapes and simplifies PATHs for installation
    Rather than declaring every path multiple times, use variables
    and ensures that all paths being used handle then between slashes
    so we do not mess something up - specially directories with spaces.
    
    Defines 'set -e' so if a command fails, the whole script stops.
    
    Removes commented code and refactors some code to be more 'bash'
    such as the 'for' loop and conditional formatting. Remove colors
    as they do not conform to the theme (light) being used - try reading
    yellow letters over a white background.

--- bin/install.sh
+++ bin/install.sh
@@ -1,43 +1,34 @@
 #!/usr/bin/env bash
-_YELLOW='\033[1;33m' # yellow color
-_GREEN='\033[0;32m' # green color
-_NC='\033[0m' # no color
 
-printf "PHPTT installer started ...\n";
+set -e
+set -o pipefail
 
-printf "Step 1: cloning the repository to ${_YELLOW}$HOME/.phptt/bin/phptt.sh${_NC}\n";
-if [ -d "$HOME/.phptt" ]; then
-    rm -rf $HOME/.phptt;
+[[ $DEBUG ]] && set -x
+
+REPO_URL="https://github.com/PHPTestFestBrasil/phptt"
+REPO_DEST="${HOME}/.phptt2"
+
+echo "PHPTT installer started ..."
+echo "Step 1: cloning the repository to $REPO_DEST"
+if [ -d $REPO_DEST ]
+then
+    rm -rf $REPO_DEST
 fi
-git clone https://github.com/PHPTestFestBrasil/phptt $HOME/.phptt;
+git clone $REPO_URL $REPO_DEST
+pushd $REPO_DEST
 
-printf "Step 2: linking files \n"
-
-files=("phptt-generate" "phptt-lcov" "phptt-test" "phptt");
-
-for ((x=0; x < ${#files[*]}; x++)) ; do
-    printf "* ${_YELLOW} "${files[$x]}"${_NC} to ${_YELLOW}/usr/local/bin/${files[$x]}${_NC} ...\n";
-    if [ -L "/usr/local/bin/${files[$x]}" ] ; then
-        rm "/usr/local/bin/${files[$x]}";
+echo "Step 2: linking files "
+for f in "phptt-generate" "phptt-lcov" "phptt-test" "phptt"
+do
+    pushd bin
+    file_source="$(pwd)/${f}"
+    file_destination="/usr/local/bin/${f}"
+    echo "*  "${file_source}" to ${file_destination} ..."
+    if [ -L "${file_destination}" ]
+    then
+        rm "${file_destination}"
     fi
-    ln -s "$HOME/.phptt/bin/${files[$x]}.sh" "/usr/local/bin/${files[$x]}";
+    ln -s "${file_source}.sh" "${file_destination}"
+    chmod a+x "${file_destination}"
+    popd
 done
-
-
-printf "Step 3: applying exec permissions ...\n";
-for ((x=0; x < ${#files[*]}; x++)) ; do
-    chmod +x "/usr/local/bin/${files[$x]}";
-    printf "${_GREEN}Success! ${_NC}The ${_YELLOW}${files[$x]}${_NC} command was added to your ${_YELLOW}/usr/local/bin${_NC} folder and can be used globally.\n";
-done
-
-
-#print "${_YELLOW}phptt.sh${_NC} to ${_YELLOW}/usr/local/bin/phptt${_NC} ...\n";
-#if [ -L "/usr/local/bin/phptt" ] ; then
-#    rm /usr/local/bin/phptt;
-#fi
-#ln -s $HOME/.phptt/bin/phptt.sh /usr/local/bin/phptt;
-#
-#printf "Step 3: applying exec permissions ...\n";
-#chmod +x /usr/local/bin/phptt;
-#
-#printf "${_GREEN}Success! ${_NC}The ${_YELLOW}phptt${_NC} command was added to your ${_YELLOW}/usr/local/bin${_NC} folder and can be used globally.\n";
