b906c2a863ecf284a092606d1ec296bc82901f5f
Rodrigo Prado de Jesus <royopa@gmail.com>
2019-04-14 15:56:34 -0300

    added php-generate phptt on bin folder

--- bin/phptt-generate.sh
+++ bin/phptt-generate.sh
@@ -33,5 +33,5 @@ function executeGenerate()
     parseGenerateArgs ${_COMMAND_ARGS};
     fixGenerateDir;
     docker run -u php --rm -i -w /usr/src/phpt -v ${_GENERATE_DIR}:/usr/src/phpt phptestfestbrasil/phptt:${_GENERATE_VERSION} \
-        php /usr/src/php/scripts/dev/generate-phpt.phar ${_GENERATE_ARGS} | sed "s/php generate-phpt.php /phptt/";
+        php generate-phpt.phar ${_GENERATE_ARGS} | sed "s/php generate-phpt.php /phptt/";
 }
